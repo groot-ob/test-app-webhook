9438e1fc5bb25b90311b9c8027d69095af8a11c0
groot-ob <brianonchari28@gmail.com>
2025-07-15 10:50:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log('Send back message to prompt response from user')
   console.log(JSON.stringify(req.body, null, 2));
   res.status(200).end();
 });
